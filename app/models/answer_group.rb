befc8859aba148814eb67ab0bf0b3c3a017ddaf9
linkinmedo <fdsmohammed@gmail.com>
2016-11-14 11:55:45 +0200

    Add some validations

--- app/models/answer_group.rb
+++ app/models/answer_group.rb
@@ -3,7 +3,7 @@ class AnswerGroup < ApplicationRecord
   belongs_to :form
   has_many :answers, dependent: :destroy, :inverse_of => :answer_group
 
-  accepts_nested_attributes_for :answers, :allow_destroy => true
+  accepts_nested_attributes_for :answers, :allow_destroy => true, :reject_if => proc { |attributes| attributes['submitted_answer'].blank? }
 
 
 end
--- app/models/form.rb
+++ app/models/form.rb
@@ -4,7 +4,7 @@ class Form < ApplicationRecord
   has_many :questions, dependent: :destroy, :inverse_of => :form
   has_many :answer_groups, dependent: :destroy, :inverse_of => :form
 
-  accepts_nested_attributes_for :questions, :allow_destroy => true
+  accepts_nested_attributes_for :questions, :allow_destroy => true, :reject_if => proc { |attributes| attributes['question_text'].blank? }
   accepts_nested_attributes_for :answer_groups, :allow_destroy => true
 
   scope :sorted, lambda { order("created_at DESC") }
